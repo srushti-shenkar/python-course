1baea7311ccbe29dd1e45f4d401aa08e817982cc
srushti-shenkar <satbshenkar1@gmail.com>
2025-04-22 14:55:23 +0530

    submit

---
+++ module 1/lesson1/activity.py
@@ -0,0 +1,12 @@
+# Printing a greeeting message
+print("Hello,world!")
+
+#Printing my name
+print("my name is srushti")
+
+#Printing a fun fact about myself
+print("I love playing soccer")
+
+#Performking and printing a simple calculation
+print("the result of 3+5 is:",3+5)
+
