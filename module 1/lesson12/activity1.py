2437b9680b36c5b4c42df3047e0017ead7582d33
srushti-shenkar <satbshenkar1@gmail.com>
2025-06-03 14:57:20 +0530

    submit

---
+++ module 1/lesson12/activity1.py
@@ -0,0 +1,14 @@
+#take input of a word
+string=input("please enter your own word:")
+#take input of a character
+char=input("please enter your own character:")
+i=0
+count=0
+#loop will not find the occurence of character
+while(i<len(string)):#string operation
+    if(string[i]==char):#condition 1 
+        count=count+1
+    i=i+1
+
+#display the result
+print("the total number of times",char,"has occured=",count)
---
+++ module 1/lesson12/activity3.py
@@ -0,0 +1,27 @@
+#input a number
+num=int(input("enter the number:"))
+t=num
+numLen=0
+#iterate the loop
+while t>0:
+    numLen=numLen+1
+    t=int(t/10)
+
+if numLen>=4:#condition 1
+    numLen=int(numLen/2)
+    chk=0
+    while num>0:#iterate loop
+        rem=num%10
+        if chk==numLen:#nested condition 1
+            midOne=rem
+        elif chk==(numLen-1):
+            midTwo=rem
+        num=int(num/10)
+        chk=chk+1
+    prod=midOne*midTwo#products of middle digits
+        #display the results
+    print("\nProduct of Mid digits("+str(midOne)+"*"+str(midTwo)+")=",prod)
+    
+else:
+    print("\nIt's not a 4 or more than 4-digit number!")
+
