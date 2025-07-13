b4c45b2921d578395d618cce9f560815610f3cbc
srushti-shenkar <satbshenkar1@gmail.com>
2025-07-13 21:09:31 +0530

    submit

---
+++ module3/lesson5/activity1.py
@@ -0,0 +1,14 @@
+#take user input
+a=input("enter a word:")
+#program to check break keyword
+for i in a: #iterate for loop
+    if(i=='A'):#condition 1
+    #display result
+        print("A is found")
+        break #break statement
+    else:
+    #display result
+        print("A not found")
+
+
+
