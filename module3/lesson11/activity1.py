9565e4c6f5170bb26c24db4630910132d390be4a
srushti-shenkar <satbshenkar1@gmail.com>
2025-10-17 20:31:12 +0530

    submit

---
+++ module3/lesson11/activity1.py
@@ -0,0 +1,14 @@
+num={1,2,3,4,5}
+print(num)
+mix_set={1,"srushti",3.0,(4,"engineer")}
+dup_set={1,2,3,4,5}
+print(dup_set)
+
+my_set=set([1,2,3,4,5])
+print(my_set)
+
+num_set=set([1,2,3,4,5])
+print(num_set)
+
+num_set.pop()
+print(num_set)
