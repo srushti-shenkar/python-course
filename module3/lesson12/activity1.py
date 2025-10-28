2421c55a28620dd1919d38f7fe76bcd506950a5b
srushti-shenkar <satbshenkar1@gmail.com>
2025-10-28 21:41:41 +0530

    submit

---
+++ module3/lesson12/activity1.py
@@ -0,0 +1,11 @@
+#python prgram to demonstraATE 
+#exit()
+for i in range(10):
+    #if the value of i becomes
+    #5 then the program is forced
+    #to exit
+    if i==5:
+        #print the exit message
+        print(exit)
+        exit()
+    print(i)
